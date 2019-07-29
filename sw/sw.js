499547b962469fc35be00bc8b5507bc2718355e1
Muneera-Salah <muneera.salah@gmail.com>
2019-07-30 01:05:32 +0300

    Offline Availability

---
+++ sw/sw.js
@@ -0,0 +1,62 @@
+var CACHE_NAME = 'mws-restaurant-stage-1-cache-v1';
+
+var urlsToCache = [
+    '/',
+    'css/styles.css',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jp',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'sw/sw-reg.js'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+
+    var cacheWhitelist = ['index-cache-v1', 'restaurant-cache-v1'];
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
